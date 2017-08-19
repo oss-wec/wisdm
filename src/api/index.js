39b07114395af9ab9204cb31a12e8e510688ff37
kissmygritts <kissmygritts08@gmail.com>
2017-08-18 17:13:22 -0700

    use html5 history mode

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:1313'
-  // baseURL: 'http://34.208.55.29:1313'
+  // baseURL: 'http://localhost:1313'
+  baseURL: 'http://34.208.55.29:1313'
 })
 
 export function getSpecies () {
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import NewAnimal from '@/components/NewAnimal'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
